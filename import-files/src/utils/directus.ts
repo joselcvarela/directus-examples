9f9981c237a038a8e7fd532a0c9ddfe694cbc449
Jose Varela <joselcvarela@gmail.com>
2025-01-21 05:06:34 +0000

    Add more utils functions

--- import-files/src/utils/directus.ts
+++ import-files/src/utils/directus.ts
@@ -1,6 +1,9 @@
 import {
   createDirectus,
+  createItem,
   DirectusClient,
+  readFieldsByCollection,
+  readRelations,
   rest,
   RestClient,
   staticToken,
@@ -36,5 +39,38 @@ export class Directus {
     formData.append("file", mockFile);
 
     return this.client.request(utilsImport(collection, formData));
+  }
+
+  async relations(collection: string) {
+    const allRelations = await this.client.request(readRelations());
+    const relations = allRelations.filter(
+      (r) =>
+        r?.meta?.many_collection === collection ||
+        r?.meta?.one_collection === collection
+    );
+
+    return relations.reduce((acc, r) => {
+      let key = "";
+
+      if (r?.meta?.many_collection === collection) key = r.meta.many_field;
+      else if (r?.meta?.one_collection === collection) key = r.meta.one_field;
+
+      if (!key) return acc;
+
+      return { ...acc, [key]: r };
+    }, {});
+  }
+
+  async createItem(collection: string, data: Record<string, any>) {
+    return this.client.request(createItem(collection, data));
+  }
+
+  async fields(collection: string) {
+    return this.client.request(readFieldsByCollection(collection));
+  }
+
+  async primaryKey(collection: string) {
+    const fields = await this.fields(collection);
+    return fields.find((f) => f.schema.is_primary_key);
   }
 }
